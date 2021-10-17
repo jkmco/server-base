d7586a9d4c972639b12d564fb6483547de6bc251
jackal <jackcakal@gmail.com>
2021-10-18 00:56:02 +0800

    Revert "connect to dydx"
    This reverts commit d18feb0413f238cf1aed283435d5394cce4c17fd.

--- index.js
+++ index.js
@@ -1,58 +1,32 @@
 // modules
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const {
   getAllAppConfig,
   getAppConfig,
   getAppConfigValue,
   saveAppConfig,
   deleteAppConfig,
-} = require('./services/appConfigService');
+} = require("./services/appConfigService");
 
 // dotenv config
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
-const DB_URI = process.env.DB_URI || 'mongodb://localhost/your_db_name';
-const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY;
-const ETH_ADDRESS = process.env.ETH_ADDRESS;
+const DB_URI = process.env.DB_URI || "mongodb://localhost/your_db_name";
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// route
+// const appConfig = require("./routes/AppConfig.route");
+// app.use("/app/config", appConfig);
+
 // startup
-require('./startup/db')(DB_URI);
+require("./startup/db")(DB_URI);
 
 // main
-
-const initAccount = require('./services/initAccount');
-
-(async () => {
-  const { client: clientA, positionId } = await initAccount(
-    ETH_ADDRESS,
-    ETH_PRIVATE_KEY
-  );
-
-  const orderParams = {
-    market: 'BTC-USD',
-    side: 'BUY',
-    type: 'LIMIT',
-    timeInForce: 'GTT',
-    postOnly: true,
-    size: '0.001',
-    price: '60000',
-    limitFee: '0.0015',
-    expiration: '2022-12-21T21:30:20.200Z',
-  };
-
-  const orderResponse = await clientA.private.createOrder(
-    orderParams,
-    positionId
-  );
-
-  console.log(orderResponse);
-})();
 
 // listen to server
 app.listen(PORT, () => {
